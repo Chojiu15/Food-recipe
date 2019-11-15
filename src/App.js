8062504dcea47a942f2d23d05ef203825557c75c
chojiu <yacine@wildcodeschool.fr>
2019-11-15 10:44:40 +0100

    Food recipe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Recipe from './Recipe'
 
-function App() {
+
+
+const App = () => {
+
+  const API_ID = 'bc7a9ea8'
+  const API_KEY = 'aeeb79e4ea32cf8eb4687aee4313b164'
+  const example = `https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+
+  const [recipe, setRecipe] = useState([])
+  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('chicken')
+  
+  useEffect(() => {
+    getRecipe()
+  }, [query])
+
+  const getRecipe = () =>{
+    axios.get(`https://api.edamam.com/search?q=${query}&app_id=${API_ID}&app_key=${API_KEY}`)
+    .then(response => setRecipe(response.data.hits))
+  }
+
+  const getSearch = (event) =>{
+    setSearch(event.target.value)
+  }
+
+  const changeQuery = (event) =>{
+    event.preventDefault()
+    setQuery(search)
+    setSearch('')
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form onSubmit={changeQuery} >
+          <input type="text" value={search} onChange={getSearch} />
+          <button type="submit">Search</button>
+        </form>
+        {recipe.map(x =>(
+          <Recipe key={x.recipe.label} title={x.recipe.label} calories={x.recipe.calories} image={x.recipe.image} />
+        ))}
     </div>
   );
 }
